85b426077a4355af360adee9772c9ed6f0c961a6
Simon Greener <simon@spdba.com.au>
2019-08-25 09:58:27 +1000

    2019-08-25 Simon Greener - Cleaned up HTML generation

--- Oracle/trunk/OBJECTS/create_multidoc_documentation.cmd
+++ Oracle/trunk/OBJECTS/create_multidoc_documentation.cmd
@@ -1,6 +1,10 @@
 @ECHO ON
 SETLOCAL ENABLEDELAYEDEXPANSION
 
+ECHO Delete any files that might be in the documentation\MULTI directory.
+DEL /Q documentation\MULTI\* 
+
+ECHO Generate new documentation html ...
 F:\Projects\database\code\robodoc-win32-4.99.36\robodoc ^
       --src src ^
       --doc documentation\MULTI ^
@@ -13,46 +17,37 @@ F:\Projects\database\code\robodoc-win32-4.99.36\robodoc ^
       --no_subdirectories ^
       --documenttitle "SPDBA Object Types and Methods Documentation" 
 
-Echo Remove any html directory not deleted by previous processing.
-mkdir html 
-REM >nul 2>&1
-ECHO Copy head_favicon.sed to html directory ...
-copy /S head_favicon.sed html
-ECHO Change to html directory ...
-cd html
-ECHO Delete any files that might be in the html directory.
-DEL /Q .\* 
-
+ECHO Change to documentation\MULTI directory
+cd documentation\MULTI
+ECHO Copy down SED script that modifies HTML ...
+COPY ..\..\head_favicon.sed .
+ECHO Set Path...
 SET PATH=%PATH%;"C:\Program Files (x86)\GnuWin32\bin"
-
-ECHO Rename documentation files to remove _sql in name...
-ECHO And modify HEAD of files.
-SET fname=_
-FOR %%a IN ( ..\documentation\MULTI\T_*.html ) DO CALL :BODY %%a
-GOTO :EOF
-
-:BODY
-  ECHO BODY....
-  SET fName=%~n1
-  ECHO ... old file name is %fName% ...
-  SET newFName=%fname:_sql=%
-  ECHO ... new file name is %newFName% ...
-  ECHO .........Copying %fName% to create %newFName%
-  COPY ..\documentation\MULTI\%fName%.html c_%newFName%.html
-  IF EXIST %newFName%.html (
-    sed -r -f head_favicon.sed c_%newFName%.html > %fName%.html
-    IF EXIST %fName%.html (
-      copy %fileName%.html ..\documentation\Oracle\MULTI
-      del  %fileName%.html 
-    ) 
-  )
-GOTO :EOF
-
-:EOF
-cd ..
-rmdir html 
-
+ECHO Modify HEAD of files to include icon ...
+sed -r -f head_favicon.sed COGO_sql.html                > COGO.html
+sed -r -f head_favicon.sed Package_COGO_sql.html        > Package_COGO.html
+sed -r -f head_favicon.sed Package_DEBUG_sql.html       > Package_DEBUG.html
+sed -r -f head_favicon.sed Package_PRINT_sql.html       > Package_PRINT.html
+sed -r -f head_favicon.sed Package_ST_LRS_sql.html      > Package_ST_LRS.html
+sed -r -f head_favicon.sed Package_TOOLS_sql.html       > Package_TOOLS.html
+sed -r -f head_favicon.sed T_Bearing_Distance_sql.html  > T_Bearing_Distance.html
+sed -r -f head_favicon.sed T_ElemInfo_sql.html          > T_ElemInfo.html
+sed -r -f head_favicon.sed T_Geometry_sql.html          > T_Geometry.html
+sed -r -f head_favicon.sed T_Grid_sql.html              > T_Grid.html
+sed -r -f head_favicon.sed T_MBR_sql.html               > T_MBR.html
+sed -r -f head_favicon.sed T_Segment_sql.html           > T_Segment.html
+sed -r -f head_favicon.sed T_Vector3D_sql.html          > T_Vector3D.html
+sed -r -f head_favicon.sed T_VertexList_sql.html        > T_VertexList.html
+sed -r -f head_favicon.sed T_Vertex_sql.html            > T_Vertex.html
+sed -r -f head_favicon.sed t_PrecisionModel_sql.html    > T_PrecisionModel.html
+sed -r -f head_favicon.sed masterindex.html             > c_masterindex.html
+DEL /Q masterindex.html
+RENAME c_masterindex.html masterindex.html
+ECHO Delete old files ...
+DEL *_sql.html
+ECHO Change back to starting directory ...
+cd ../..
+ECHO Quitting ...
 ENDLOCAL
 pause
-exit
 
