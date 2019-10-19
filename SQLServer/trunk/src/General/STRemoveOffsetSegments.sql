3a3f8a115d6f0e1694f9f18b420089b5f0570762
Simon Greener <simon@spdba.com.au>
2019-10-19 17:27:31 +1100

    2019-10-19 Simon Greener - Added STRemoveOffsetSegments. This function checks start and end segments of a line being offset to see if they "disappear" and so need to be removed.

---
+++ SQLServer/trunk/src/General/STRemoveOffsetSegments.sql
@@ -0,0 +1,65 @@
+use DEVDB
+go
+
+drop function [dbo].[STRemoveOffsetSegments] 
+go
+
+create function [dbo].[STRemoveOffsetSegments] (
+  @p_linestring      geometry,
+  @p_offset_distance float,
+  @p_round_xy        int,
+  @p_round_zm        int
+)
+returns geometry
+As
+Begin
+  Declare
+    @v_linestring  geometry,
+   @v_buffer      geometry,
+   @v_buffer_ring geometry;
+
+    IF ( @p_linestring.STNumPoints() = 2 ) 
+      Return @p_linestring;
+
+    -- Get start and end segments that don't disappear.
+    SET @v_buffer      = @p_linestring.STBuffer(ABS(@p_offset_distance));
+    SET @v_buffer_ring = @v_buffer.STExteriorRing();
+
+   -- TODO: Need to extend to all pairs within linestring.
+    WITH segments as (
+      SELECT s.id, s.geom as segment,
+             s.geom.STNumPoints() as numPoints
+        FROM [dbo].[STSegmentLine](@p_linestring) as s
+    ), ids as (
+     SELECT MIN(id) as minId, MAX(id) as maxId
+       FROM (SELECT /* original Line as offset segments */
+                    s.[id],
+                   ROUND([dbo].[STParallelSegment] (
+                                   s.segment,
+                                   @p_offset_distance,
+                                   @p_round_xy,
+                                   @p_round_zm
+                          ).ShortestLineTo(@v_buffer_ring)
+                     .STLength(),
+                          @p_round_xy
+                    ) as Dist2Boundary
+               FROM segments s
+           ) as f
+       WHERE f.Dist2Boundary = 0.0
+    )
+    SELECT @v_linestring = [dbo].[STMakeLineFromGeometryCollection] ( 
+                                    geometry::CollectionAggregate ( f.line ),
+                                    @p_round_xy,
+                                    @p_round_zm 
+                           )
+      FROM (SELECT TOP (100) PERCENT
+                   s.segment as line 
+              FROM ids as i
+                     INNER JOIN
+                       segments as s 
+                         ON (s.id between i.minId and i.maxId)
+             ORDER BY s.id
+          ) as f;
+   return @v_linestring;
+End
+go
